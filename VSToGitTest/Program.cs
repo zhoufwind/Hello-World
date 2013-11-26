2bf907b199638364e342b590437b683268848501
Stephen Zhou <zhoufwind@hotmail.com>
2013-11-26 20:58:07 +0800

    TEST: Adding a project from VS to Git

---
+++ VSToGitTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSToGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is just a test for uploading project to GitHub");
+            Console.ReadLine();
+        }
+    }
+}
