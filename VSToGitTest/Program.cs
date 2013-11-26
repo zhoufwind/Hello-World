ff63917c7e128f504da9297ec2819bc1f5029cc3
Stephen Zhou <zhoufwind@hotmail.com>
2013-11-26 21:14:06 +0800

    Adding some symbol

--- VSToGitTest/Program.cs
+++ VSToGitTest/Program.cs
@@ -11,6 +11,7 @@ namespace VSToGitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hi, this is just a test for uploading project to GitHub");
+            Console.Writeline("*******************************************************");
             Console.ReadLine();
         }
     }
